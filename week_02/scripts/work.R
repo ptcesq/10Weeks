fbc585446a70e9155a4be713c9b9bc09ff058bcb
Patrick T. Cronin <ptcesq@gmail.com>
2015-07-13 11:25:11 -0400

    Updated Work Script; added model and ROC function.

--- week_02/scripts/work.R
+++ week_02/scripts/work.R
@@ -1,8 +1,57 @@
+# set seed 
+set.seed(1234)
+
 
 # Load data into system 
-spam <- read.csv("C:/Users/Patrick/OneDrive/R_Projects/10_Weeks/week_02/data/spambase.data", header=FALSE)
+spam <- read.csv("./data/spambase.data", header=FALSE)
 
 # add column names 
-cols <- read.table("C:/Users/Patrick/OneDrive/R_Projects/10_Weeks/week_02/data/cols.txt", quote="\"", stringsAsFactors=FALSE)
+cols <- read.table("./data/cols.txt", quote="\"", stringsAsFactors=FALSE)
 colnames(spam) <- cols$V1
+rm(cols)
+
+# create test and training sets 
+inTrain <- sample(1:nrow(spam), nrow(spam) * 0.85) # select 85% of the items 
+train <- spam[inTrain, ]
+test <- spam[-inTrain, ]
+rm(spam, inTrain)
+
+# Lets start with a standard glm 
+model <- glm(train$spam ~ ., data=train)
+predicted <- predict(model, test, "response" )
+results <- as.data.frame(cbind(test$spam, predicted))
+colnames(results) <- c("actual", "predicted")
+
+library(ROCR)
+# precision recall graph 
+pred <- prediction(results$predicted, results$actual)
+
+## computing a simple ROC curve (x-axis: fpr, y-axis: tpr)
+perf <- performance(pred,"tpr","fpr")
+auc.perf <- performance(pred, measure="auc")
+auc.perf@y.values
+ROC.Val <- auc.perf@y.values
+main.label <- paste("ROC Curve - AUC=", ROC.Val)
+plot(perf, colorize=TRUE, main=main.label)
+abline(a=0, b=1)
+
+## precision/recall curve (x-axis: recall, y-axis: precision)
+perf1 <- performance(pred, "prec", "rec")
+plot(perf1)
+
+## sensitivity/specificity curve (x-axis: specificity,
+## y-axis: sensitivity)
+perf2 <- performance(pred, "sens", "spec")
+plot(perf2)
+
+
+
+
+
+
+
+
+
+
+
 
