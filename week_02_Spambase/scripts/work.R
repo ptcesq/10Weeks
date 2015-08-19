d37d88cd77d3503c354840f0e6c0f992d837f17f
Patrick T. Cronin <ptcesq@gmail.com>
2015-08-19 08:07:34 -0400

    worked on script.

--- week_02_Spambase/scripts/work.R
+++ week_02_Spambase/scripts/work.R
@@ -3,13 +3,13 @@ set.seed(6678)
 
 
 # Load data into system 
-spam <- read.csv("./data/spambase.data", header=FALSE)
+spam <- read.csv("D:\\R_Projects\\10weeks\\10Weeks\\week_02_Spambase\\data\\spambase.data", header=FALSE)
 
 # add column names 
-cols <- read.table("./data/cols.txt", quote="\"", stringsAsFactors=FALSE)
+cols <- read.table("D:\\R_Projects\\10weeks\\10Weeks\\week_02_Spambase\\data\\cols.txt", quote="\"", stringsAsFactors=FALSE)
 colnames(spam) <- cols$V1
 rm(cols)
-# spam$class <- factor(spam$class, labels=c("spam", "ham"))
+spam$class <- factor(spam$class, labels=c("spam", "ham"))
 
 
 # create test and training sets 
@@ -26,14 +26,20 @@ colnames(results) <- c("actual", "odds")
 results$pred <- ifelse(results$odds < 0.5, 0, 1)
 
 
+# Contingency Table 
+library(gmodels)
+with(results, CrossTable(actual, pred))
+with(results, CrossTable(pred, actual, prop.chisq=FALSE, 
+                         prop.r=FALSE, prop.c=FALSE, prop.t=FALSE, 
+                         format="SPSS"))
+
+
+
 
 library(ROCR)
 # precision recall graph 
-<<<<<<< HEAD
-pred <- prediction(results$actual, results$pred)
-=======
 pred <- prediction(results$pred, results$actual)
->>>>>>> 9c901e741a6a380e30a72c8ba7709a0f5cbbfb52
+
 
 ## computing a simple ROC curve (x-axis: fpr, y-axis: tpr)
 perf <- performance(pred,"tpr","fpr")
@@ -47,18 +53,10 @@ abline(a=0, b=1)
 
 # Contingency Table 
 library(gmodels)
-<<<<<<< HEAD
 with(results, CrossTable(actual, pred))
-=======
 with(results, CrossTable(pred, actual, prop.chisq=FALSE, 
                          prop.r=FALSE, prop.c=FALSE, prop.t=FALSE, 
                          format="SPSS"))
->>>>>>> 9c901e741a6a380e30a72c8ba7709a0f5cbbfb52
-
-
-
-
-
 
 
 
