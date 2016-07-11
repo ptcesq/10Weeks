0614a20e118242775f64aa825eacd8ec382b6ea9
ptcesq <ptcesq@gmail.com>
2016-07-11 09:03:06 -0400

    rework sample

--- week_13_Tick_Tock/scripts/generate sample.R
+++ week_13_Tick_Tock/scripts/generate sample.R
@@ -1,7 +1,15 @@
 
-d <- data.frame(date = c('6-6-16', '6-7-16', '6-8-16', '6-9-16', '6-10-16'))
+## read in date/time info in format 'm/d/y h:m:s'
+dates <- c("06/06/2016", "06/07/2016", "06/08/2016", "06/09/2016", "06/10/2016")
+times <- c("08:00:00", "08:00:00", "08:00:00", "08:00:00", "08:00:00")
+x <- paste(dates, times)
+ d <- strptime(x, "%m/%d/%Y %H:%M:%S")
+
 n = 100
 t1 <- do.call("rbind", replicate(n, d, simplify = FALSE))
+t1 <- as.data.frame(t1)
+
+
 
 e <- data.frame(employee = c('a', 'b', 'c', 'd', 'e'))
 n = 100
