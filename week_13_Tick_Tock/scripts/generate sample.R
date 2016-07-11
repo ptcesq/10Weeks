1ae704ea909dcb00792aecbd05120408632bb9ad
ptcesq <ptcesq@gmail.com>
2016-07-11 09:21:08 -0400

    worked generator

--- week_13_Tick_Tock/scripts/generate sample.R
+++ week_13_Tick_Tock/scripts/generate sample.R
@@ -3,11 +3,10 @@
 dates <- c("06/06/2016", "06/07/2016", "06/08/2016", "06/09/2016", "06/10/2016")
 times <- c("08:00:00", "08:00:00", "08:00:00", "08:00:00", "08:00:00")
 x <- paste(dates, times)
- d <- strptime(x, "%m/%d/%Y %H:%M:%S")
+d <- strptime(x, "%m/%d/%Y %H:%M:%S")
 
 n = 100
-t1 <- do.call("rbind", replicate(n, d, simplify = FALSE))
-t1 <- as.data.frame(t1)
+cards <- do.call("rbind", replicate(n, d, simplify = FALSE))
 
 
 
@@ -15,4 +14,6 @@ e <- data.frame(employee = c('a', 'b', 'c', 'd', 'e'))
 n = 100
 t2 <- do.call("rbind", replicate(n, e, simplify = FALSE))
 
-t3 <- cbind(t1, t2)
+cards <- cbind(cards, t2)
+
+# end
