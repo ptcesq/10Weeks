a3b2bd4dfeae9f3c18934bd40c19e246c8163c2d
Patrick T Cronin <ptcesq@gmail.com>
2017-01-22 06:00:34 -0500

    Finished up german credit file (Week 7).

--- week_07_Credit/scripts/work.R
+++ week_07_Credit/scripts/work.R
@@ -1,35 +1,37 @@
 # Set Seed 
 set.seed(1234)
 
-library(readr)
-data <- read_delim("~/r_projects/10Weeks/week_07_Credit/data/german.csv",
-                    " ", escape_double = FALSE, trim_ws = TRUE)
+# read the data 
+data <- read.csv('./data/german.csv', stringsAsFactors = FALSE, 
+                 sep=' ', header = FALSE)
 
 # Read in Column Names 
-columns <- read.csv('./data/cols.txt')
-colnames(data) <- columns$col_name
+columns <- read.csv('./data/cols.txt', stringsAsFactors = FALSE)
+colnames(data) <- trimws(columns$col_name)
 
-# Fetch Codes 
-codes <- read.csv('./data/codes.txt')
+codes <- read.csv('./data/codes.txt', stringsAsFactors = FALSE)
+codes[,2] <- trimws(codes[,2])
 
+# Replace Codes 
+for (i in 1:nrow(codes)){
+  current_col = codes[i, 1]
+  current_code = codes[i, 2]
+  current_replace = codes[i, 3]
+  data[, current_col] <- gsub(current_code, current_replace, data[,current_col])
+}
 
+factor_cols <- unique(codes[,1])
+for (cur_col in factor_cols){
+  data[, cur_col] <- as.factor(data[,cur_col])
+}
 
-# rename target as target 
-colnames(data)[61] <- "target"
+data$target <- ifelse(data$target == 1, TRUE, FALSE)
+data$target <- as.factor(data$target)
 
-#
-# set threshold 
-# or transform dicotomous 
-#
-data$target <- as.factor(ifelse(data$target <= 1400, 0, 1))
-rm(cols)
-
-# 
-# In this data set we need to remove the URL (adds nothing to the model and confuses it) 
-#
-data <- subset(data, select = -c(url))
-
-
+# Clean up the workspace 
+rm(codes, columns, cur_col, 
+   current_code, current_replace, 
+   factor_cols, i, current_col)
 
 # create test and training sets 
 inTrain <- sample(1:nrow(data), nrow(data) * 0.85) # select 85% of the items 
@@ -63,7 +65,7 @@ pred <- prediction(results$pred, results$actual)
 ## computing a simple ROC curve (x-axis: fpr, y-axis: tpr)
 perf <- performance(pred,"tpr","fpr")
 auc.perf <- performance(pred, measure="auc")
-auc.perf@y.values
+print(paste('Accuracy: ', auc.perf@y.values))
 ROC.Val <- auc.perf@y.values
 main.label <- paste("ROC Curve - AUC=", ROC.Val)
 plot(perf, colorize=TRUE, main=main.label)
