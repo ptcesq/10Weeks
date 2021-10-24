18e9b6d3b827c6329ecbbfda31ddc82065098c6e
ptcesq <patrick_t_cronin@msn.com>
2021-10-24 13:45:19 +0000

    Added ploting

--- week_01_SMS/scripts/py_work.py
+++ week_01_SMS/scripts/py_work.py
@@ -15,6 +15,14 @@ classes = df['class'].tolist()
 ham_class = classes.count('ham') 
 spam_class = classes.count('spam') 
 
+# Plot the results 
+labels = ['good', 'spam']
+
+values = [ham_class, spam_class]
+fig1, ax1 = plt.subplots()
+ax1.pie(values, labels=labels, autopct='%1.1f%%')
+plt.show() 
+
 
 
 
